c636ce5541ec0b961243c02c93bee5ed2a1d3c76
Celestine Akpanoko <celestineakpanoko@gmail.com>
2025-03-26 11:26:07 -0500

    setting up deployment needs and organization

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,40 @@
 // Next configuration file
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  devIndicators: {
-    // Disable the build indicator
-    appIsrStatus: false,
+// const nextConfig: NextConfig = {
+//   devIndicators: false,
+//   // devIndicators: {
+//   //   // Disable the build indicator
+//   //   appIsrStatus: false,
+//   // },
+// };
+
+// export default nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  devIndicators: false,
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [
+      'clerk.com', 
+      'images.clerk.dev',
+      'storage.googleapis.com'
+    ]
   },
-};
+  output: 'standalone',
+  webpack: (config) => {
+    // Handle Google API issues
+    config.resolve.fallback = { 
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false
+    };
+    return config;
+  }
+}
 
-export default nextConfig;
+module.exports = nextConfig
