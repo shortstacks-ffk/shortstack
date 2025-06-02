544c96ccf6620b027109c49e8b05c6641e34724c
Celestine Akpanoko <celestineakpanoko@gmail.com>
2025-06-02 12:02:14 -0500

    improve class lesson plan ui

--- src/app/(teacher dashboard)/teacher/dashboard/classes/[classId]/lesson-plans/[planId]/page.tsx
+++ src/app/(teacher dashboard)/teacher/dashboard/classes/[classId]/lesson-plans/[planId]/page.tsx
@@ -1,5 +1,5 @@
 // app/dashboard/classes/[classId]/[planId]/page.tsx
-import LessonPlanDetailPage from './LessonPlanDetailPage';
+import ClassLessonPlanDetailPage from './ClassLessonPlanDetailPage';
 
 interface PageProps {
   params: Promise<{ classId: string; planId: string }>;
@@ -11,6 +11,6 @@ export default async function Page({ params }: PageProps) {
 
   // Render the client component, passing in classId & planId as props
   return (
-    <LessonPlanDetailPage classId={classId} planId={planId} />
+    <ClassLessonPlanDetailPage classId={classId} planId={planId} />
   );
 }
